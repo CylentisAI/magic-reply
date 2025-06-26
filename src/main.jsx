c1bf0849196f8e51918eb3fc9b18e472ed9c00d8
CylentisAI <aaron@cylentis.com>
2025-06-26 13:35:44 +1000

    Update main.jsx
    working app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-export default function App(){return <h1 className="text-3xl">It works!</h1>}
-ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+import AppShell from "./ui/AppShell";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AppShell />
+  </React.StrictMode>
+);
