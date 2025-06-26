cfff306df5077f7ec58ba26267fa4d2adf6318ba
CylentisAI <aaron@cylentis.com>
2025-06-26 13:18:20 +1000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+export default function App(){return <h1 className="text-3xl">It works!</h1>}
+ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
